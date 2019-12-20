00fd6655fac8ddcbbe69594d70a8738f71a9ba8d
Amit Tiwari <smilyamitt@gmail.com>
2019-12-20 19:44:08 +0100

    updated server

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
