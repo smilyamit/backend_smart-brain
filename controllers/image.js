fc47ccd6fada055a492369274b03f74c8777bdb7
Amit Tiwari <smilyamitt@gmail.com>
2019-12-21 23:53:58 +0100

    deployed front end

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: 'cd425d073e8445ed918e9b15ea1adee4'
 });
 
 const handleApiCall = (req, res) => {
