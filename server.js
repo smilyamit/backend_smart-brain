606a56cf6f070e5e2810b26923cb8b6c5edfef37
Amit Tiwari <smilyamitt@gmail.com>
2019-12-22 02:53:29 +0100

    sdfd

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send() })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
